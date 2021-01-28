51e1e13a4e12ee3bc171496c84ecf31a3a1c7b04
Zulham Azwar Achmad <zulham724@students.unnes.ac.id>
2021-01-28 11:22:19 +0700

    update socket with room chat purpose

--- socket/index.js
+++ socket/index.js
@@ -1,12 +1,48 @@
 const { io } = require("../bin/www");
 const socketioJwt = require('socketio-jwt')
 
-io.on(
-    "connection",
-    socketioJwt.authorize({
-        secret: "YXp3YXI="
+// io.on(
+//     "connection",
+//     socketioJwt.authorize({
+//         secret: "YXp3YXI="
+//     })
+// ).on("authenticated", (socket) => {
+//     //this socket is authenticated, we are good to handle more events from it.
+//     console.log(`hello! ${socket.decoded_token.name}`);
+// });
+
+io.on("connection", socket => {
+
+    // untuk masuk ke room
+    socket.on('create', function(room) {
+        console.log('room joined with name', room)
+        socket.join(room);
     })
-).on("authenticated", (socket) => {
-    //this socket is authenticated, we are good to handle more events from it.
-    console.log(`hello! ${socket.decoded_token.name}`);
-});
+
+    // untuk event pesan
+    socket.on('message', ({ room, data }) => {
+        socket.to(room).emit('message', data);
+        console.log(room, data)
+    })
+
+    // untuk event mengetik
+    socket.on('typing', ({ room }) => {
+        socket.to(room).emit('typing', 'Someone is typing')
+    })
+
+    // untuk event stop mengetik
+    socket.on('stopped_typing', ({ room }) => {
+        socket.to(room).emit('stopped_typing')
+    })
+
+    // ganti event dari pusher
+    socket.on('assigment_event', ({ room, data }) => {
+        socket.to(room).emit('assigment_event', data)
+    })
+
+})
+
+// io.sockets.to('notification').emit('LikedPost', 'what is going on, party people?');
+
+// // this message will NOT go to the client defined above
+// io.sockets.in('foobar').emit('message', 'anyone in this room yet?');
